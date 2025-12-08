e4788da1a0e548186db506e139ef614915a30595
Sabrina <sabrinalamprecht92@gmail.com>
2025-12-08 10:51:46 -0800

    server overload potential fix

---
+++ create_tables.py
@@ -0,0 +1,10 @@
+# create_tables.py
+
+from app import create_app
+from app.models import db
+
+app = create_app('ProductionConfig')
+
+with app.app_context():
+    db.create_all()  
+    print("✅ All tables created successfully")
